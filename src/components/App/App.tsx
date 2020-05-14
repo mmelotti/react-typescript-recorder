d3a2d33490d2d7039b61f902f7fed489530a935f
Maison Melotti <maisonmelotti@gmail.com>
2020-05-14 14:19:10 -0300

    loading events test

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,18 +2,12 @@ import React from 'react';
 import './App.css';
 import Recorder from '../Recorder';
 import Calendar from '../Calendar';
-import MaterialDatePicker from '../MaterialDatePicker';
-import Button from '@material-ui/core/Button';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Recorder />
       <Calendar />
-      <MaterialDatePicker />
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
     </div>
   );
 };
