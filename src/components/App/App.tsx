b1cc0c0197540109a4fd1e452fb49213ff4377bd
Maison Melotti <maisonmelotti@gmail.com>
2020-05-14 11:06:45 -0300

    init new component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Recorder from '../Recorder';
 import Calendar from '../Calendar';
+import MaterialDatePicker from '../MaterialDatePicker';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Recorder />
       <Calendar />
+      <MaterialDatePicker />
     </div>
   );
 };
